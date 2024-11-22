fe201ec87dc0b43877cd8232720e8db3efe26171
Dominique <dlmagat08@proton.me>
2024-11-22 11:53:58 +0800

    dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="flex items-center justify-center">Dashboard</h1>
+      <h1 className="text-center text-5xl font-bold">Dashboard</h1>
       {/* charts */}
     </>
   );
