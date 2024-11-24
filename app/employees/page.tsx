7e9fd728e1e140a19d087d3080c7627cc03d8833
Doms <dlmagat08@proton.me>
2024-11-24 21:03:19 +0800

    added dynamic route

--- app/employees/page.tsx
+++ app/employees/page.tsx
@@ -1,9 +1,10 @@
+import Link from "next/link";
 import React from "react";
 
 function page() {
     return <>
         <h1 className="text-center text-2xl md:text-5xl font-bold">Employees</h1>
-
+        <Link href="/employees/1/">User 1</Link>
     </>;
 
 
