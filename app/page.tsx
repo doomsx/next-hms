0bc8d771804046bae2f652afea4de6cd12f6bb65
Dominique <dlmagat08@proton.me>
2024-12-05 16:41:46 +0800

    dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
-export default function Home() {
-    return (
-        <>
-            <h1 className="text-center text-2xl md:text-5xl font-bold">Dashboard</h1>
-            {/* charts */}
-        </>
-    );
+import { getUsers } from "@/lib/db";
+
+export default async function Home() {
+  const data = await getUsers();
+  console.log(data);
+  return (
+    <>
+      <h1 className="text-center text-2xl md:text-5xl font-bold">Dashboard</h1>
+      {/* charts */}
+      <p>Total Active Employees : {data.length}</p>
+    </>
+  );
 }
